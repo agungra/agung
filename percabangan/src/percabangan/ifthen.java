e18cf6585810e24c6c27a183f710936acc061a5d
agungra <74241702+agungra@users.noreply.github.com>
2020-11-10 05:27:07 -0800

    percabangan

---
+++ percabangan/src/percabangan/ifthen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percabangan;
+
+/**
+ *
+ * @author Winbit
+ */
+public class ifthen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean isOn = true;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        }
+    }
+    }
+    
+
---
+++ percabangan/src/percabangan/ifthenelse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percabangan;
+
+/**
+ *
+ * @author Winbit
+ */
+public class ifthenelse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      boolean isOn = false;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        } else {
+            System.out.println("Kondisi tidak terpenuhi...");
+        }
+    }
+    }
+    
+

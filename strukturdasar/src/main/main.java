e4da17c016f29848b9b8ee2afd76dd9be63d732c
agungra <74241702+agungra@users.noreply.github.com>
2020-11-10 05:29:06 -0800

    strukturdasar

---
+++ strukturdasar/src/main/main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import musik.gitar;
+import kendaraan.kereta;
+import kendaraan.motor;
+import kendaraan.mobil;
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        gitar.bunyi();
+        kereta.jumlahBan();
+        mobil.jumlahBan();
+        motor.jumlahBan();
+        
+    }
+    }
+    
+
---
+++ strukturdasar/src/musik/gitar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musik;
+
+/**
+ *
+ * @author Winbit
+ */
+public class gitar {
+    public static void bunyi(){
+        System.out.println("jrenggg..");
+    }
+}
